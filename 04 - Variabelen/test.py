2565f545288183e4d8ec819ad8848d302e5bd08b
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-09-19 11:36:12 +0200

    Patroontje

---
+++ 04 - Variabelen/test.py
@@ -0,0 +1,14 @@
+# Vraag coëfficiënten bij x^2, x^1 en x^0 van een 2de graadsfunctie f(x) = a*x^2 + b*x^1 + c*x^0
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
+
+# Bereken de discriminant
+discriminant = b ** 2 - (4 * a * c)
+
+# uitvoer
+print('discriminant = ', discriminant)
+
+# ------------------------------------
+
+print('discriminant =', float(input("b: ")) ** 2 - (4 * float(input("a: ")) * float(input("c: "))))
