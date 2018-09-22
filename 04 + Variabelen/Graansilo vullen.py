1437d182261c0a900c80a95e976307d066ba55da
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-09-22 22:17:12 +0200

    Graansilo vullen

--- 04 + Variabelen/Graansilo vullen.py
+++ 04 + Variabelen/Graansilo vullen.py
@@ -1,6 +1,6 @@
 # Inputs
-b = float(input("Breedte: ")) / 100
-l = float(input("Lengte: ")) / 100
+b = float(input("Breedte: "))
+l = float(input("Lengte: "))
 c = float(input("Graan per hectare in m^3: "))
 r = float(input("Straal van de silo: "))
 h = float(input("Hoogte van de silo: "))
@@ -8,11 +8,15 @@ h = float(input("Hoogte van de silo: "))
 # pi
 pi = 3.1415926535897931
 
+# Fractions
+teller = (b * l * c)
+noemer = (pi * (r ** 2) * h) * 10000
+
 # Berekening silo's
-aantal_silos = int((b * l * c) / (pi * (r ** 2) * h) + 0.9999999999999999)
+aantal_silos = int(teller / noemer + 0.99999999999)
 
 # Berekening hoogte
-hoogte_silo = (b * l * c) / (pi * (r ** 2) * h) * h % h
+hoogte_silo = (teller / (pi * (r ** 2) * 10000)) % h
 
 # Print
 print(str(aantal_silos))
