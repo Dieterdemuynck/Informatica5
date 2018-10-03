df1efd446aa75bdb4123a08ffacd726ddea09a84
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-10-03 11:37:53 +0200

    Crocodile Paradox

---
+++ 06 - Condities/Crocodile Paradox.py
@@ -0,0 +1,14 @@
+# Indien vader werd opgegeven, print je: Krokodil geeft kind terug.
+# Indien krokodil werd opgegeven, print je: Krokodil eet kind op.
+# In alle andere geval toon je: Moe van het denken.
+
+naam = input("Wie heeft stalen zenuwen? ")
+
+if naam == "vader":
+    boodschap = "Krokodil geeft kind terug"
+elif naam == "krokodil":
+    boodschap = "Krokodil eet kind op"
+else:
+    boodschap = "Moe van het denken"
+
+print(boodschap)
---
+++ 06 - Condities/test.py
@@ -0,0 +1,10 @@
+x = int(input("Geef een geheel getal: "))
+
+if x % 2 == 0 or x % 3 == 0:
+    print(x, "is een veelvoud van 2 of 3")
+
+elif x % 6 == 0:
+    print(x, "is unreachable!! THIS WILL NEVER BE PRINTED!! MWUAHAHAHAHAHAAA")
+
+else
+    print("Not veelvoud of 2 or 3 much thank.")
