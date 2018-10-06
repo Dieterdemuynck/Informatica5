e0b921210f5fdb5a7b2657266d3a74f6e8e6a5af
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-10-06 09:54:24 +0200

    Blad steen schaar

--- 06 - Condities/Blad steen schaar.py
+++ 06 - Condities/Blad steen schaar.py
@@ -1,9 +1,13 @@
 hand_1 = input("Hand van speler 1? ")
 hand_2 = input("Hand van speler 2? ")
 
+blad_v_steen = hand_1 == "blad" and hand_2 == "steen"
+steen_v_schaar = hand_1 == "steen" and hand_2 == "schaar"
+schaar_v_blad = hand_1 == "schaar" and hand_2 == "blad"
+
 if hand_1 == hand_2:
     uitkomst = "onbeslist"
-elif (hand_1 == "blad" and hand_2 == "steen") or (hand_1 == "steen" and hand_2 == "schaar") or (hand_1 == "schaar" and hand_2 == "blad"):
+elif (blad_v_steen == True) or (steen_v_schaar == True) or (schaar_v_blad == True):
     uitkomst = "speler 1 wint"
 else:
     uitkomst = "speler 2 wint"
