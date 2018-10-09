63f8db53479dd379504dd2732a21f6d88c26a77e
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-10-09 19:43:09 +0200

    Codontype.py

---
+++ 06 + Condities/Codontype.py
@@ -0,0 +1,12 @@
+codon = input("Codon: ")
+
+if len(codon) != 3:
+    output = "ongeldig"
+elif codon == "AUG":
+    output = "start"
+elif codon == "UAG" or codon == "UGA" or codon == "UAA":
+    output = "stop"
+else:
+    output = "gewoon"
+
+print("Het codon", codon, "is een", output, "codon.")
