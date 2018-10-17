4d87d9e1036426773263792cc81099e2050749aa
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-10-17 14:22:38 +0200

    gotta push

--- 07a - Iteraties - ForLus/Biggest number and average.py
+++ 07a - Iteraties - ForLus/Biggest number and average.py
@@ -1,13 +1,14 @@
 aantal = int(input("Aantal getallen: "))
 
-maximum = -100
-som = 0
+maximum = int(input("Eerste getal: "))
+som = maximum
 
-for aantal in range(1, (aantal + 1)):
+for aantal in range(2, (aantal + 1)):
 
-    getal = int(input("Getal: "))
+    getal = int(input("Volgend getal: "))
     maximum = max(getal, maximum)
 
     som += getal
+
 gemiddelde = round((som / aantal), 2)
 print("Het grootste getal is {} en het gemiddelde is {}".format(maximum, gemiddelde))
