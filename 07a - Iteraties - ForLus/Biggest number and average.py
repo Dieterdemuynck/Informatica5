616e5c56d9561e307095d7779aea5b676429897d
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-10-24 10:38:43 +0200

    PUSH DE WAGON

--- 07a - Iteraties - ForLus/Biggest number and average.py
+++ 07a - Iteraties - ForLus/Biggest number and average.py
@@ -3,7 +3,7 @@ aantal = int(input("Aantal getallen: "))
 maximum = int(input("Eerste getal: "))
 som = maximum
 
-for aantal in range(2, (aantal + 1)):
+for i in range(aantal - 1):
 
     getal = int(input("Volgend getal: "))
     maximum = max(getal, maximum)
