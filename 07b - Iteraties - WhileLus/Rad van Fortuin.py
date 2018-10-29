da8a4050b54f7f94e2b4bda579561684dd61ef4d
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-10-29 15:11:31 +0100

    Not out of tiem anymore

--- 07b - Iteraties - WhileLus/Rad van Fortuin.py
+++ 07b - Iteraties - WhileLus/Rad van Fortuin.py
@@ -1,2 +1,13 @@
 woord = input("verborgen woord: ")
 geldbedrag = int(input("Gedraaide geldbedrag: "))
+
+letter = input("Geef een letter: ")
+gebruikt = ""
+som = 0
+
+while letter in woord and not letter in gebruikt:
+    som += geldbedrag
+    gebruikt += letter
+    letter = input("Geef een letter: ")
+
+print(som)
