47c9abcdda4e95fa9b6b1e98c6165ab79b42085d
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-11-05 19:07:55 +0100

    Let's hope for the best.

--- 07b - Iteraties - WhileLus/Priemgetallen.py
+++ 07b - Iteraties - WhileLus/Priemgetallen.py
@@ -2,13 +2,21 @@ getal = int(input("Geef me eens jouw mooi getalletje: "))
 i = 2
 priemgetal = 1
 
-if getal < 2 or getal % i == 0:
+if getal < i:
+    priemgetal = 0
+elif getal == i:
+    priemgetal = 1
+elif getal % i == 0:
     priemgetal = 0
 
-while getal % i != 0 and i < getal - 1:
+while getal % i != 0 and i - 1 < getal:
     i += 1
-    if getal % i == 0:
+    if getal == i:
+        priemgetal = 1
+    elif getal % i == 0:
         priemgetal = 0
+    else:
+        priemgetal = 1
 
 if priemgetal:
     print("{} is een priemgetal".format(getal))
