47de7d8cd324b962843b7fb2c3ee4db341dc8de2
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-11-14 10:56:24 +0100

    Comet

--- Toets 1/Perfecte getallen.py
+++ Toets 1/Perfecte getallen.py
@@ -1,4 +1,13 @@
-getal = int(input("Geef een getal: "))
+getal = int(input("Een getal: "))
 
-for i in range(1, getal):
-    if i
+delersom = 0
+delers = ""
+for i in range(1, (getal // 2) + 1):
+    if getal % i == 0:
+        delers += str(i) + " "
+        delersom += i  # De som van de delers moet gelijk zijn aan het oorspronkelijk getal
+
+if delersom == getal:  # Zie vorige comment (line 8)
+    print("{} is een perfect getal en de delers zijn {}".format(getal, delers))
+else:
+    print("{} is geen perfect getal".format(getal))
