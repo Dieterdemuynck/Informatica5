4341125d1e2713c9cfd0e8f1787fa5d6cb291d43
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-11-14 11:17:31 +0100

    Comet

---
+++ Thuisbestanden/testfile_1.py
@@ -0,0 +1,16 @@
+# Idea: Make a tiny RPG-game through python.
+# Images are not necessary, therefor try to make it through text only.
+
+### POINT SYSTEM ###
+# HP: Health Points of a character.
+# MP: Magic Points (used for spells).
+# LP: Luck Points, these will determine your "luck" with random-chance-encounters and looting chests, etc.
+
+### EXTRA IDEAS ###
+# Items: You can find items such as weapons, scrolls, armor, etc in chests or from loot drops, for use in combat.
+# LV: Level of a character. These determine the strength of someone's attacks, defense, etc.
+
+### GENERAL GAMEPLAY ###
+# Still up for decision:
+#   Pick a "class" and a number of monsters to fight
+#       The player gets his stats through his class, can upgrade a little between each fight.
