e0fde2a46f5e6db23bb2f765f489a13864fd1332
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-11-21 23:19:42 +0100

    Class decision added

--- Thuisbestanden/testfile_1.py
+++ Thuisbestanden/testfile_1.py
@@ -14,3 +14,22 @@
 # Still up for decision:
 #   Pick a "class" and a number of monsters to fight
 #       The player gets his stats through his class, can upgrade a little between each fight.
+
+from random import randint, random
+
+### CHARACTER ###
+character = input("What class would you like to be? Choose between: Warrior, Mage, Rogue:  ")
+
+character_decided = False
+while not character_decided:
+    if character == "Mage" or character == "mage":
+
+        character_decided = True
+    elif character == "Warrior" or character == "warrior":
+
+        character_decided = True
+    elif character == "Rogue" or character == "rogue":
+
+        character_decided = True
+    else:
+        character = input("That was not a valid class. Please choose between Mage, Warrior or Rogue:  ")
