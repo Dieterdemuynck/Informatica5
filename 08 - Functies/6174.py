6208616740f8be5e007e72212c7b7be3d46e02c3
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2018-11-21 23:02:25 +0100

    splits opgelost

--- 08 - Functies/6174.py
+++ 08 - Functies/6174.py
@@ -1,1 +1,12 @@
 def splits(getal):
+    cijfer1, cijfer2, cijfer3, cijfer4 = -1, -1, -1, -1
+    for i in str(getal):
+        if cijfer1 < 0:
+            cijfer1 = int(i)
+        elif cijfer2 < 0:
+            cijfer2 = int(i)
+        elif cijfer3 < 0:
+            cijfer3 = int(i)
+        else:
+            cijfer4 = int(i)
+    return cijfer1, cijfer2, cijfer3, cijfer4
