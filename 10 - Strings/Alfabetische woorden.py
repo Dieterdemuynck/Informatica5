9030904812c7de5f4408cd4997cf56c2c5ca904d
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2019-01-02 01:07:20 +0100

    I dont really know what to put here... I mean, what I make here has literally no use to anyone except myself, and the programs I write aren't really that important or hard to make either... So yea, have this giant-ass commit message instead! Have a good day/night!

--- 10 - Strings/Alfabetische woorden.py
+++ 10 - Strings/Alfabetische woorden.py
@@ -1,1 +1,22 @@
 def positie_laagste_ascii(string):
+    character_min = ord(string[0])
+    for character in string:
+        character_min = min(character_min, ord(character))
+
+    return string.find(chr(character_min))
+
+
+def sorteer(string):
+    string_sorted = ""
+    for _ in string:
+        string_sorted += string[positie_laagste_ascii(string)]
+        string = string[:positie_laagste_ascii(string)] + string[positie_laagste_ascii(string)+1:]
+
+    return string_sorted
+
+
+def is_alfabetisch(string):
+    if string == sorteer(string):
+        return True
+    else:
+        return False
