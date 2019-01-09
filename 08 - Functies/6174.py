537a42a3f8cf57acc32d6bf2321da56c02dc6f09
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2019-01-09 11:10:59 +0100

    kaprekar verbeterd

--- 08 - Functies/6174.py
+++ 08 - Functies/6174.py
@@ -1,14 +1,9 @@
 def splits(getal):
-    cijfer1, cijfer2, cijfer3, cijfer4 = -1, -1, -1, -1
-    for i in str(getal):
-        if cijfer1 < 0:
-            cijfer1 = int(str(i))
-        elif cijfer2 < 0:
-            cijfer2 = int(i)
-        elif cijfer3 < 0:
-            cijfer3 = int(i)
-        else:
-            cijfer4 = int(i)
+    cijfer1 = int(str(getal)[0])
+    cijfer2 = int(str(getal)[1])
+    cijfer3 = int(str(getal)[2])
+    cijfer4 = int(str(getal)[3])
+
     return cijfer1, cijfer2, cijfer3, cijfer4
 
 
@@ -18,12 +13,18 @@ def oplopende_cijfers(a, b, c, d):
     cijfer3 = max(min(a, b), min(c, d), min(a, d), min(b, c), min(a, c), min(b, d))
     cijfer4 = max(a, b, c, d)
 
+    # kandidaat1 = max(min(a, b), min(a, c), min(b, c))
+    # kandidaat2 = a + b + c + d - cijfer1 - kandidaat1 - cijfer4
+
+    # cijfer2 = min(kandidaat1, kandidaat2)
+    # cijfer3 = max(kandidaat1, kandidaat2)
+
     return cijfer1, cijfer2, cijfer3, cijfer4
 
 
 def op_af_getallen(a, b, c, d):
     cijfer_oplopend = str(a) + str(b) + str(c) + str(d)
-    cijfer_aflopend = str(d) + str(c) + str(b) + str(a)
+    cijfer_aflopend = str(d) + str(c) + str(b) + str(a) # cijfer_aflopend = cijfer_oplopend[::-1]
     return cijfer_oplopend, cijfer_aflopend
 
 
@@ -39,9 +40,9 @@ def kaprekar(getal):
         cijfer1, cijfer2, cijfer3, cijfer4 = oplopende_cijfers(opgesplitst1, opgesplitst2, opgesplitst3, opgesplitst4)
         cijfer_oplopend, cijfer_aflopend = op_af_getallen(cijfer1, cijfer2, cijfer3, cijfer4)
         getal = int(verschil(cijfer_aflopend, cijfer_oplopend))
-        string += "\n{} - {} = {}".format(cijfer_aflopend, cijfer_oplopend, getal)
+        string += "{} - {} = {}\n".format(cijfer_aflopend, cijfer_oplopend, getal)
 
-    return string
+    return string[:-1]
 
 
 print(kaprekar(1234))
