85cb01be5bb6a268532688c0e46ff84c869f18be
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2019-01-30 12:00:28 +0100

    Gotcha started: info given

---
+++ 12a - Lists/Gotcha.py
@@ -0,0 +1,11 @@
+def ik_heb_gemoord(opdrachtenlijst, moordenaar):
+    # moordenaar + 1 verwijderen
+    # moordenaar + 2 = volgende opdracht
+
+
+
+
+    # return (volgende_opdracht, opdrachtenlijst)
+
+
+def ik_ben_vermoord(opdrachtenlijst, slachtoffer):
