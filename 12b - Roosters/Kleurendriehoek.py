3e9f616c621aa5ce1a56586cd6014bfdce833d88
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2019-02-06 21:40:57 +0100

    Kleurendriehoek = Done

--- 12b - Roosters/Kleurendriehoek.py
+++ 12b - Roosters/Kleurendriehoek.py
@@ -11,3 +11,27 @@ def volgende_rij(rij):
             volgende_rij.append("G")
 
     return volgende_rij
+
+
+def driehoek(rij):
+    driehoek = [rij]
+    for i in range(1, len(rij)):
+        driehoek.append(volgende_rij(driehoek[i - 1]))
+
+    return driehoek
+
+
+def kleuren(driehoek):
+    g = 0
+    y = 0
+    r = 0
+    for i in range(len(driehoek)):
+        for j in range(len(driehoek[i])):
+            if driehoek[i][j] == "G":
+                g += 1
+            elif driehoek[i][j] == "R":
+                r += 1
+            elif driehoek[i][j] == "Y":
+                y += 1
+
+    return g, r, y
