5db86c93fbc40980bf07377ce8b5e76f4b274494
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2019-02-18 22:52:09 +0100

    Woorden tellen = Done

--- 13 - Dictionaries/Woorden tellen.py
+++ 13 - Dictionaries/Woorden tellen.py
@@ -15,4 +15,22 @@ def woord_frequentie(text):
 
 
 def woorden_per_frequentie(text):
+    # woorden tellen
     aantallen = woord_frequentie(text)
+
+    gesorteerd = {}
+
+    # Keys en values "omwisselen", als value er al in zit: woord in lijst toevoegen
+    for key, value in aantallen.items():
+        if value in gesorteerd:
+            gesorteerd[value].append(key)
+        else:
+            gesorteerd[value] = [key]
+
+    return gesorteerd
+
+
+def meest_gebruikte_woorden(text):
+    frequentie_woorden = woorden_per_frequentie(text)
+
+    return frequentie_woorden[max(frequentie_woorden)]
