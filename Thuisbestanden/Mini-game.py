dc61327e7698a360f325c39e56c8e7efd7612cc4
Dieterdemuynck <42997575+Dieterdemuynck@users.noreply.github.com>
2019-05-15 11:20:07 +0200

    Adjusted mini-game cheatcodes

--- Thuisbestanden/Mini-game.py
+++ Thuisbestanden/Mini-game.py
@@ -3,8 +3,20 @@ from random import randint
 number = randint(0, 100)
 
 cheats_answer = input("Hello! Would you like to enter a cheatcode? \n")
-if cheats_answer == "Yes" or cheats_answer == "yes" or cheats_answer == "y":
+if cheats_answer.lower() == "yes" or cheats_answer.lower() == "y":
     password = input("Enter the cheatcode: \n")
+
+    if password.lower() == "hayasaka ftw":
+        print("The number you're looking for is", number, ", you majestic being.")
+    elif password.lower() == "raphtalia":
+        range_1 = number - randint(0, 20)
+        range_2 = number + randint(0, 20)
+        print("Your number is somewhere between", range_1, "and", range_2, ".")
+    else:
+        print("that wasn't a correct cheatcode. Try again next game.")
+else:
+    print("Then we'll start immediatly!")
+
 
 guess = int(input("Guess the number: \n"))
 tries = 1
@@ -19,10 +31,14 @@ while guess != number:
     tries += 1
 
 print("Congratulations! The number was indeed", number, ".")
-print("It took you", tries, "tries to get it.")
-if tries > 10:
-    print("That could be better...")
-elif tries == 10:
-    print("That's a decent job.")
+
+if tries == 1:
+    print("It took you", tries, "try to get it.\nThat's amazing! Lucky you!")
 else:
-    print("That's a good job!")
+    print("It took you", tries, "tries to get it.")
+    if tries > 10:
+        print("That could be better...")
+    elif tries == 10:
+        print("That's a decent job.")
+    else:
+        print("That's a good job!")
